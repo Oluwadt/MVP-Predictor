38a8d26db70f43971362a2ac8c6e831569bd98da
Oluwadt <63118746+Oluwadt@users.noreply.github.com>
2024-12-09 22:47:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,4 @@ with st.expander('Data'):
   y
 
 with st.expander('Data Visualization'):
-  st.scatter_chart(data=df, x='MIN', y='PTS', color='RANK', size='RANK')
+  st.scatter_chart(data=df, x='MIN', y='PTS', color='RANK')
