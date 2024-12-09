1ab9ddbb5fdebe90989d5006dfc808c4d40af55e
Oluwadt <63118746+Oluwadt@users.noreply.github.com>
2024-12-09 21:50:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🏀 NBA MVP Predictor')
 
 st.info("This app predicts the players most likely to win this year's NBA MVP award.")
+
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/Oluwadt/NBA-MVP-Predictor/refs/heads/main/combined_past_data.csv')
+  df
