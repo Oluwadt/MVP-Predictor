ea0ae0f16c40d21eee21b52a49613c1a45af6528
Oluwadt <63118746+Oluwadt@users.noreply.github.com>
2024-12-11 11:07:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,30 +20,29 @@ with st.expander('Data'):
   y
 
 with st.expander('Data Visualization'):
-  category_names = {1: "Low", 2: "High", 3: "Very High"}
-  df["Category Name"] = df["RANK"].map(category_names)
+  # category_names = {1: "Low", 2: "High", 3: "Very High"}
+  # df["Category Name"] = df["RANK"].map(category_names)
 
-  fig, ax = plt.subplots()
-  categories = df["Category Name"].unique()
-  colors = plt.cm.get_cmap("tab10", len(categories))
+  # fig, ax = plt.subplots()
+  # categories = df["Category Name"].unique()
+  # colors = plt.cm.get_cmap("tab10", len(categories))
 
-  for i, category in enumerate(categories):
-      cat_data = df[df["Category Name"] == category]
-      ax.scatter(
-        cat_data["EFF"], 
-        cat_data["FG_PCT"], 
-        label=f"{category}", 
-        color=colors(i),
-        edgecolor='none',
-        s=50
-      )
+  # for i, category in enumerate(categories):
+  #     cat_data = df[df["Category Name"] == category]
+  #     ax.scatter(
+  #       cat_data["EFF"], 
+  #       cat_data["FG_PCT"], 
+  #       label=f"{category}", 
+  #       color=colors(i),
+  #       edgecolor='none',
+  #       s=50
+  #     )
 
-  plt.ylim(0, 1.5)
-  # plt.xlim(1990, 2025)
-  # Add labels, title, and legend
-  ax.set_xlabel("Efficiency", fontsize=12)
-  ax.set_ylabel("PPG", fontsize=12)
-  ax.legend(title="MVP Chance", title_fontsize=12, fontsize=10, loc="upper left", frameon=False)
+  # plt.ylim(0, 1.5)
+  # # Add labels, title, and legend
+  # ax.set_xlabel("Efficiency", fontsize=12)
+  # ax.set_ylabel("PPG", fontsize=12)
+  # ax.legend(title="MVP Chance", title_fontsize=12, fontsize=10, loc="upper left", frameon=False)
 
-  st.pyplot(fig)
-  # st.scatter_chart(data=df, x='YEAR', y='PTS', color='RANK')
+  # st.pyplot(fig)
+  st.scatter_chart(data=df, x='YEAR', y='PTS', color='RANK')
