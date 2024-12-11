b66a25f589ea262ec8f9de8fd6ac0f1148d8c221
Oluwadt <63118746+Oluwadt@users.noreply.github.com>
2024-12-11 11:41:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,34 +23,7 @@ with st.expander('Data'):
 with st.expander('Data Visualization'):
   category_labels = {1: "Low", 2: "High", 3: "Very High"}
   df["MVP Chance"] = df["RANK"].map(category_labels)
-  # category_names = {1: "Low", 2: "High", 3: "Very High"}
-  # df["Category Name"] = df["RANK"].map(category_names)
-
-  # fig, ax = plt.subplots()
-  # categories = df["Category Name"].unique()
-  # colors = plt.cm.get_cmap("tab10", len(categories))
-
-  # for i, category in enumerate(categories):
-  #     cat_data = df[df["Category Name"] == category]
-  #     ax.scatter(
-  #       cat_data["EFF"], 
-  #       cat_data["FG_PCT"], 
-  #       label=f"{category}", 
-  #       color=colors(i),
-  #       edgecolor='none',
-  #       s=50
-  #     )
-
-  # plt.ylim(0, 1.5)
-  # # Add labels, title, and legend
-  # ax.set_xlabel("Efficiency", fontsize=12)
-  # ax.set_ylabel("PPG", fontsize=12)
-  # ax.legend(title="MVP Chance", title_fontsize=12, fontsize=10, loc="upper left", frameon=False)
-
-  # st.pyplot(fig)
   
-  # st.scatter_chart(data=df, x='YEAR', y='PTS', color="MVP Chance")
-
   # Define x-axis range
   x_min, x_max = 1990, 2025  # Customize the range as needed
   
@@ -60,8 +33,6 @@ with st.expander('Data Visualization'):
       y="PTS",
       color=alt.Color("MVP Chance", title="MVP Chance"),  # Custom legend
       tooltip=["YEAR", "PTS", "MVP Chance"]  # Optional: Add tooltips
-  ).properties(
-      title="Scatter Chart with Custom X-Axis Range"
   )
   
   # Display the chart in Streamlit
