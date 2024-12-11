b143fabeb0dbd3744cb4b234767d1f8a5b1c16a1
Oluwadt <63118746+Oluwadt@users.noreply.github.com>
2024-12-11 22:25:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,8 +63,9 @@ with st.sidebar:
 player_df
 st.markdown(f"""
 <div style="padding: 1.5rem; background-color: #f9fbfc; border-radius: 0.5rem; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
-    <h3 style="margin: 0; color: #4CAF50;">{player_df['PLAYER'].values[0]} Year: {player_df['YEAR']}</h3>
-    <p style="margin: 0.5rem 0 0; color: #333;">PPG: {player_df['PTS']} APG: {player_df['AST']} RPG: {player_df['REB']}
-    BLK: {player_df['BLK']} STL: {player_df['STL']} TOV: {player_df['TOV']}</p>
+    <h3 style="margin: 0; color: #4CAF50;">{player_df['PLAYER'].values[0]} Year: {player_df['YEAR'].values[0]}</h3>
+    <p style="margin: 0.5rem 0 0; color: #333;">PPG: {player_df['PTS'].values[0]}\n APG: {player_df['AST'].values[0]} 
+    RPG: {player_df['REB'].values[0]}
+    BLK: {player_df['BLK'].values[0]} STL: {player_df['STL'].values[0]} TOV: {player_df['TOV'].values[0]}</p>
 </div>
 """, unsafe_allow_html=True)
